318adb34c1cc18e54c568e3485f46dc1f941347a
guipleit <pipisibr@live.com>
2022-11-30 16:44:17 -0300

    adiciona exercicio leitura de objetos etapa 2

--- fundamentos/secao-04-introducao-a-javascript/secao-4-4/exercicios/leituraDeObjetos.js
+++ fundamentos/secao-04-introducao-a-javascript/secao-4-4/exercicios/leituraDeObjetos.js
@@ -14,3 +14,17 @@ let leitor = {
 //Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
 
 console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0].titulo}'`);
+
+//Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto contendo as seguintes informações:
+//{
+  //titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  //autor: 'JK Rowling',
+  //editora: 'Rocco',
+
+
+leitor.livrosFavoritos[1] = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+}
+console.log(leitor);
