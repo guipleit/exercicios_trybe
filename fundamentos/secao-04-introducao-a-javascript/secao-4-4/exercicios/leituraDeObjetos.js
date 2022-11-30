1bd8c7198bea76654321717c109b1cf4c9335e91
guipleit <pipisibr@live.com>
2022-11-30 16:50:12 -0300

    adiciona exercicio leitura de objetos etapa 3

--- fundamentos/secao-04-introducao-a-javascript/secao-4-4/exercicios/leituraDeObjetos.js
+++ fundamentos/secao-04-introducao-a-javascript/secao-4-4/exercicios/leituraDeObjetos.js
@@ -28,3 +28,7 @@ leitor.livrosFavoritos[1] = {
     editora: 'Rocco',
 }
 console.log(leitor);
+
+//Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
+
+console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos`);
