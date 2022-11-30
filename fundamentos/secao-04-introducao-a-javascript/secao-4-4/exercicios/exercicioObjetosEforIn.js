434b3aca8811b5e298d987fdd7b5b1bc606008c6
guipleit <pipisibr@live.com>
2022-11-30 16:14:07 -0300

    adiciona exercicio objetos e for in etapa 5

--- fundamentos/secao-04-introducao-a-javascript/secao-4-4/exercicios/exercicioObjetosEforIn.js
+++ fundamentos/secao-04-introducao-a-javascript/secao-4-4/exercicios/exercicioObjetosEforIn.js
@@ -31,3 +31,21 @@ for (let key in info) {
 for (let keyValor in info) {
   console.log(info[keyValor]);
 }
+
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. 
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+}
+
+for (let key in info2) {
+  if(key === 'recorrente' && info[key] && info2[key]) {
+    console.log("Ambos recorrentes!");
+  } else {
+    console.log(`${info[key]} // ${info2[key]}`);
+  }
+
+}
