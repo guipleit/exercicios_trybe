2a6f98675fc3df30df05070dcd19993e0475a258
guipleit <pipisibr@live.com>
2022-12-01 16:15:08 -0300

    adiciona exercicio 1 Parte 2

--- fundamentos/secao-04-introducao-a-javascript/Secao-4-5/Exercicios Parte 1/exercicio7.js
+++ fundamentos/secao-04-introducao-a-javascript/Secao-4-5/Exercicios Parte 1/exercicio7.js
@@ -14,4 +14,4 @@ let speed = 90
 
 const speedCar = (speed) => (speed >= 120) ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`
 
-console.log(speedCar(150));
+console.log(speedCar(speed));
