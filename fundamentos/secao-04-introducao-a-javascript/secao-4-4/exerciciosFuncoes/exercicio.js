87d1c78b29103c4ade073527bd790d49cf5a662c
guipleit <pipisibr@live.com>
2022-12-01 10:55:33 -0300

    adiciona exercicio função 7

--- fundamentos/secao-04-introducao-a-javascript/secao-4-4/exerciciosFuncoes/exercicio.js
+++ fundamentos/secao-04-introducao-a-javascript/secao-4-4/exerciciosFuncoes/exercicio.js
@@ -111,3 +111,25 @@ return storeNum
 
 
 console.log(sumOfN(5));
+
+//Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
+
+//Valor de teste: 'trybe' e 'be'
+
+//Valor esperado no retorno da função: true
+
+//verificaFimPalavra('trybe', 'be');
+
+//Retorno esperado: true
+//verificaFimPalavra('joaofernando', 'fernan');
+
+//Retorno esperado: false
+
+function checkString(str, end) {
+let resultado = str.split(end)
+if (resultado[resultado.length - 1]  === ''){
+ return true
+}
+return false
+}
+console.log(checkString('trybe', 'be'));
