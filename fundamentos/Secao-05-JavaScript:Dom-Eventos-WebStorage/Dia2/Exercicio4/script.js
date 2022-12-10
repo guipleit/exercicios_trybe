047a15182aa72d3b31aef63d58f2fad6cc37a0af
guipleit <pipisibr@live.com>
2022-12-10 12:48:10 -0300

    adiciona exercicio 4 requisito 7

--- fundamentos/Secao-05-JavaScript:Dom-Eventos-WebStorage/Dia2/Exercicio4/script.js
+++ fundamentos/Secao-05-JavaScript:Dom-Eventos-WebStorage/Dia2/Exercicio4/script.js
@@ -8,29 +8,33 @@
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, … como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 // Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
 
-const body = document.getElementById('body')
-const bodyChild = document.createElement('h1')
-bodyChild.innerHTML = 'TrybeTrip - Agência de Viagens'
-body.appendChild(bodyChild)
+const body = document.getElementById('body');
+const bodyChild = document.createElement('h1');
+bodyChild.innerHTML = 'TrybeTrip - Agência de Viagens';
+body.appendChild(bodyChild);
 
-const tagMain = document.createElement('main')
-tagMain.className = 'main-content'
-body.appendChild(tagMain)
+const tagMain = document.createElement('main');
+tagMain.className = 'main-content';
+body.appendChild(tagMain);
 
-const tagSection = document.createElement('section')
-tagSection.className = 'center-content'
-tagMain.appendChild(tagSection)
+const tagSection = document.createElement('section');
+tagSection.className = 'center-content';
+tagMain.appendChild(tagSection);
 
-const tagP = document.createElement('p')
-tagP.innerHTML = 'ALGUM TEXTO'
-tagSection.appendChild(tagP)
+const tagP = document.createElement('p');
+tagP.innerHTML = 'ALGUM TEXTO';
+tagSection.appendChild(tagP);
 
-const leftContent = document.createElement('section') 
-leftContent.className = 'left-content'
-tagMain.appendChild(leftContent)
+const leftContent = document.createElement('section') ;
+leftContent.className = 'left-content';
+tagMain.appendChild(leftContent);
 
-const rightContent = document.createElement('section') 
-rightContent.className = 'right-content'
-tagMain.appendChild(rightContent)
+const rightContent = document.createElement('section') ;
+rightContent.className = 'right-content';
+tagMain.appendChild(rightContent);
+
+const image = document.createElement('img');
+image.src = 'https://picsum.photos/200'
+tagSection.appendChild(image)
 
 
