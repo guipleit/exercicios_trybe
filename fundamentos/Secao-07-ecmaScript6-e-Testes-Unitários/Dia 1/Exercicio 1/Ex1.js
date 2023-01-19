3f3ce489a84b33fcb6d4934668c56906741f843d
guipleit <pipisibr@live.com>
2023-01-19 09:04:14 -0300

    Adiciona ex 1

---
+++ fundamentos/Secao-07-ecmaScript6-e-Testes-Unitários/Dia 1/Exercicio 1/Ex1.js
@@ -0,0 +1,67 @@
+// //Parte II - Pedido de clientes
+// Imagine que você tenha ficado responsável por cuidar do sistema de entrega de um restaurante e que precisasse enviar mensagens para os clientes com a informação da compra. Para isso, use o seguinte código:
+
+// Complete a função customerInfo() para que seu retorno seja similar a 'Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701'.
+
+// Complete a função orderModifier() para que seu retorno seja similar a 'Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00.';
+// Modifique o nome da pessoa compradora para Luiz Silva;
+// Modifique o valor total da compra para R$ 50,00.
+
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+
+
+const customerInfo = (order) => {
+    const client = order.name
+    const employee = order.order.delivery.deliveryPerson
+    const phone = order.phoneNumber
+    const adress = order.address.street
+    const houseNumber = order.address.number
+    const complement = order.address.apartment
+// 'Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701'.
+    return `Olá ${employee}, entrega para: ${client}, Telefone: ${phone}, R. ${adress}, Nº ${houseNumber}, AP: ${complement}`
+};
+
+customerInfo(order);
+console.log(customerInfo(order));
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+
+};
+
+orderModifier(order);
