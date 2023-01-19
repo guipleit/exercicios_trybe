79a9675506826621705c892482e608393cf0f8a0
guipleit <pipisibr@live.com>
2023-01-19 09:14:55 -0300

    Adiciona ex 1

--- fundamentos/Secao-07-ecmaScript6-e-Testes-Unitários/Dia 1/Exercicio 1/Ex1.js
+++ fundamentos/Secao-07-ecmaScript6-e-Testes-Unitários/Dia 1/Exercicio 1/Ex1.js
@@ -3,9 +3,7 @@
 
 // Complete a função customerInfo() para que seu retorno seja similar a 'Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701'.
 
-// Complete a função orderModifier() para que seu retorno seja similar a 'Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00.';
-// Modifique o nome da pessoa compradora para Luiz Silva;
-// Modifique o valor total da compra para R$ 50,00.
+
 
 const order = {
   name: 'Rafael Andrade',
@@ -45,23 +43,32 @@ const order = {
 
 
 
+
 const customerInfo = (order) => {
-    const client = order.name
-    const employee = order.order.delivery.deliveryPerson
-    const phone = order.phoneNumber
-    const adress = order.address.street
-    const houseNumber = order.address.number
-    const complement = order.address.apartment
+  const client = order.name
+  const employee = order.order.delivery.deliveryPerson
+  const phone = order.phoneNumber
+  const adress = order.address.street
+  const complement = order.address.apartment
+  const houseNumber = order.address.number
 // 'Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701'.
-    return `Olá ${employee}, entrega para: ${client}, Telefone: ${phone}, R. ${adress}, Nº ${houseNumber}, AP: ${complement}`
+  return `Olá ${employee}, entrega para: ${client}, Telefone: ${phone}, R. ${adress}, Nº ${houseNumber}, AP: ${complement}`
 };
 
 customerInfo(order);
 console.log(customerInfo(order));
 
-const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+// Complete a função orderModifier() para que seu retorno seja similar a 'Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00.';
+// Modifique o nome da pessoa compradora para Luiz Silva;
+// Modifique o valor total da compra para R$ 50,00.
 
+const orderModifier = (order) => {
+    const newClient = order.name = 'Luiz Silva'
+    const newTotal = order.payment.total = 'R$ 50,00.'
+    const pizza = Object.keys(order.order.pizza)
+    const drink = order.order.drinks.coke.type
+    return `Olá ${newClient}, o total do seu pedido de ${pizza[0]}, ${pizza[1]} e ${drink} é ${newTotal}`
 };
 
 orderModifier(order);
+console.log(orderModifier(order))
