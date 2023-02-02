853e281de433ccb1c2e1671a784db54fcc43b2b8
guipleit <pipisibr@live.com>
2023-02-02 14:18:21 -0300

    adiciona requisito 3 parte 3

--- fundamentos/Secao-08-Higher-Order-Functions/Dia 1/Exercício/GameActionsSimulator.js
+++ fundamentos/Secao-08-Higher-Order-Functions/Dia 1/Exercício/GameActionsSimulator.js
@@ -30,5 +30,20 @@ const dragonAttack = (dragon) => {
 const warriorAttack = (warrior) => {
     return Math.floor(Math.random() * (warrior.strength - warrior.weaponDmg + 1) + warrior.weaponDmg)
 }
-console.log(dragonAttack(dragon));
-console.log(warriorAttack(warrior));
+
+// Crie uma função que retorne um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// A mana consumida por turno é 15. Além disso, a função deve ter uma condicional: caso o mago tenha menos de 15 de mana, o valor de dano recebe uma mensagem (Ex: “Não possui mana suficiente”), e a mana gasta é 0.
+
+const mageAttack = (mage) => {
+    let mana = mage.mana
+    let damageRoll = Math.floor(Math.random() * (mage.intelligence * 2 - mage.intelligence + 1) + mage.intelligence)
+    if (mana <= 15) {
+        return 'Não possui mana suficiente.'
+    } else {
+        mana -= 15
+        return damageRoll
+    }
+}
+
+console.log(mageAttack(mage));
