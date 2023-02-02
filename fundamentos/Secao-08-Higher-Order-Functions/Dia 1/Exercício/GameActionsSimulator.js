b3b1f54ceb05fc9d336d7c461929f637fbef73d7
guipleit <pipisibr@live.com>
2023-02-02 13:54:00 -0300

    adiciona requisito 1 parte 3

---
+++ fundamentos/Secao-08-Higher-Order-Functions/Dia 1/Exercício/GameActionsSimulator.js
@@ -0,0 +1,27 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+const randomDamage = () => {
+    return Math.floor(Math.random() * (dragon.strength - 15 + 1) + 15)
+}
+
+console.log(randomDamage());
