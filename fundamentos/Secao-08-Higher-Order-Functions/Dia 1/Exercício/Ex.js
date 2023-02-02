2ba7c5b3f43bfe41db93d89b382df2fe326cbfcd
guipleit <pipisibr@live.com>
2023-02-02 12:34:45 -0300

    adiciona ex 2

--- fundamentos/Secao-08-Higher-Order-Functions/Dia 1/Exercício/Ex.js
+++ fundamentos/Secao-08-Higher-Order-Functions/Dia 1/Exercício/Ex.js
@@ -17,4 +17,40 @@ const newEmployees = () => {
   return employees;
 };
 
-console.log(newEmployees());
+// Sorteador de loteria
+// Desenvolva uma HOF que retorne o resultado de um sorteio. Essa HOF irá gerar um número aleatório entre 1 e 5, recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns, você ganhou”).
+
+// Corretor automático de exame
+// Crie uma HOF que receberá três parâmetros:
+// O primeiro será um array de respostas corretas (soluções);
+// O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
+// O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. Para isso, essa função usará os seguintes critérios:
+// Uma resposta correta adiciona 1 ponto à pontuação final;
+// A ausência de uma resposta não altera a pontuação (quando for “N.A”);
+// Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
+// Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays:
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compare = (array1, array2) => {
+    let count = 0;
+    for (let index = 0; index < array1.length; index += 1) {
+        if (array1[index] === array2[index]) {
+            count += 1
+        }
+        if (array1[index] === 'N.A' || array2[index] === 'N.A') {
+            count
+        }
+        if (array1[index] !== array2[index]) {
+            count -= 0.5
+        }
+    }
+    return count
+}
+const autoCorrect = (rightAnswers, studentAnswer, compare) => {
+    return compare(rightAnswers, studentAnswer)
+}
+
+console.log(autoCorrect(RIGHT_ANSWERS, STUDENT_ANSWERS, compare))
+
