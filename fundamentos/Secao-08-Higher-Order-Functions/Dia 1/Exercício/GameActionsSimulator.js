a503e4f9fd724229eb1f0adebb5978091dc57359
guipleit <pipisibr@live.com>
2023-02-02 14:11:15 -0300

    adiciona requisito 2 parte 3

--- fundamentos/Secao-08-Higher-Order-Functions/Dia 1/Exercício/GameActionsSimulator.js
+++ fundamentos/Secao-08-Higher-Order-Functions/Dia 1/Exercício/GameActionsSimulator.js
@@ -24,4 +24,11 @@ const dragonAttack = (dragon) => {
     return Math.floor(Math.random() * (dragon.strength - 15 + 1) + 15)
 }
 
+// Crie uma função que retorne o dano causado pelo warrior.
+// O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
+
+const warriorAttack = (warrior) => {
+    return Math.floor(Math.random() * (warrior.strength - warrior.weaponDmg + 1) + warrior.weaponDmg)
+}
 console.log(dragonAttack(dragon));
+console.log(warriorAttack(warrior));
