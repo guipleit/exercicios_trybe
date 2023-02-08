16269d1c02095d7ff83f9e95a30a29a247434260
guipleit <pipisibr@live.com>
2023-02-08 15:42:28 -0300

    adiciona exercicio 1

--- front-end/Front-end-e-JS-assincrono/Dia 1/Ex 1/src/main.js
+++ front-end/Front-end-e-JS-assincrono/Dia 1/Ex 1/src/main.js
@@ -1,15 +1,27 @@
-import { nanoid } from 'nanoid'
-import './style.css'
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+import './style.css';
 
+// Variáveis globais
+let button = document.getElementById('btn-pass');
+let result = document.getElementById('result');
+//
 
 const generateRandomPassword = () => {
-    let button = document.getElementById('btn-pass')
-    let result = document.getElementById('result')
+    
     button.addEventListener('click', () => {
-       const randomPassword = nanoid()
-        result.innerHTML = randomPassword
-    })
-}
+        const randomPassword = nanoid();
+        result.innerHTML = randomPassword;
+    });
+};
+
+const copyOnClick = () => {
+    result.addEventListener('click', (event) => {
+        copy(event.target.innerHTML);
+        alert('Senha copiada!');
+    });
+};
 window.onload = () => {
-generateRandomPassword()
-}
+    generateRandomPassword();
+    copyOnClick();
+};
