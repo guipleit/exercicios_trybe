bdf96c8658fca30e6a17c2f584025e0d8d6480eb
guipleit <112783039+guipleit@users.noreply.github.com>
2023-02-27 19:20:59 -0300

    adiciona req 1 e 2

--- front-end/Frond-end-indroducao-ao-react/Dia 1/sd-029-b-exercise-frontend-content-main/src/App.js
+++ front-end/Frond-end-indroducao-ao-react/Dia 1/sd-029-b-exercise-frontend-content-main/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import H1Render from './Header';
+import Content from './Content';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Exercise - Frontend content</h1>
+      <>
+        <h1>Exercise - Frontend content</h1>
+        <H1Render />
+        <Content />
+      </>
     );
   }
 }
