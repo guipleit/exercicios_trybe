bd2885cd40760d054fc486238035a8a437ed10de
guipleit <112783039+guipleit@users.noreply.github.com>
2023-02-27 19:27:01 -0300

    adiciona req 3, termina exercício.

--- front-end/Frond-end-indroducao-ao-react/Dia 1/sd-029-b-exercise-frontend-content-main/src/App.js
+++ front-end/Frond-end-indroducao-ao-react/Dia 1/sd-029-b-exercise-frontend-content-main/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import H1Render from './Header';
 import Content from './Content';
+import CreateFooter from './Footer';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <h1>Exercise - Frontend content</h1>
         <H1Render />
         <Content />
+        <CreateFooter />
       </>
     );
   }
