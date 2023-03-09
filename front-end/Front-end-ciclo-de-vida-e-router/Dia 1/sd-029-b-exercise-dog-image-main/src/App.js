a5f5d665ff7ac4f9047473152c25e1b297bca5f5
guipleit <112783039+guipleit@users.noreply.github.com>
2023-03-09 20:10:03 -0300

    finaliza ex

--- front-end/Front-end-ciclo-de-vida-e-router/Dia 1/sd-029-b-exercise-dog-image-main/src/App.js
+++ front-end/Front-end-ciclo-de-vida-e-router/Dia 1/sd-029-b-exercise-dog-image-main/src/App.js
@@ -2,27 +2,68 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      dog: '',
+    };
+  }
+
   componentDidMount() {
-    // Implemente sua lógica aqui
+    this.fetchApi();
   }
 
   shouldComponentUpdate(/* nextProps, nextState */) {
     // Implemente sua lógica aqui
-    return true;
+    const { dog } = this.state;
+    return !dog.includes('terrier');
   }
 
   componentDidUpdate() {
     // Implemente sua lógica aqui
+    const { dog } = this.state;
+    localStorage.setItem('dog', dog);
+    const breedStartIndex = dog.indexOf('/breeds/') + '/breeds/'.length;
+    const breedEndIndex = dog.indexOf('/', breedStartIndex);
+    const breed = dog.substring(breedStartIndex, breedEndIndex);
+    alert(breed);
   }
 
-  async fetchApi = () => {
+  fetchApi = async () => {
     const requestResponse = await fetch('https://dog.ceo/api/breeds/image/random');
-    const returnedObj = await requestResponse.json()
+    const returnedObj = await requestResponse.json();
     console.log(returnedObj);
-  }
+    this.setState({
+      dog: returnedObj.message,
+      loading: false,
+    });
+  };
+
   render() {
-    this.fetchApi()
-    return <h1>Doguinhos</h1>;
+    const {
+      dog,
+      loading,
+    } = this.state;
+    const loadingMessage = <h1>Loading...</h1>;
+    return (
+      <div>
+        <h1>Doguinhos</h1>
+        {
+          loading ? loadingMessage : <img src={ dog } alt="Doguinho aleatório" />
+        }
+
+        <button
+          id="new-dog"
+          onClick={ () => this.fetchApi() }
+        >
+          Novo doguinho!
+
+        </button>
+
+      </div>
+    );
   }
 }
 
