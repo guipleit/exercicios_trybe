0cff59c13bab2d544afd1cd7a117804c180268d9
guipleit <112783039+guipleit@users.noreply.github.com>
2023-03-10 13:51:31 -0300

    adiciona req 7 a 9 do ex de fix

--- front-end/Front-end-ciclo-de-vida-e-router/Dia 2/exercise-react-router-main/src/App.js
+++ front-end/Front-end-ciclo-de-vida-e-router/Dia 2/exercise-react-router-main/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
+
     return (
       <BrowserRouter>
       <ul>
@@ -14,10 +15,17 @@ class App extends Component {
         <li><Link to="/about">About</Link></li>
         <li><Link to="/users">Users</Link></li>
       </ul>
+      <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/users" component={Users} />
+      <Route
+            exact
+            path="/users/:id"
+            render={ (props) => <Users {...props} greetingsMessage="Good Morning" /> }
+          />
+      </Switch>
       </BrowserRouter>
+      
     );
   }
 }
